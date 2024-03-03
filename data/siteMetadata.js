a414613b2db9a85fd25450e1c2b762dd2e123a74
June <zlxjune@gmail.com>
2024-03-04 01:18:57 +0800

    Add Cusdis comment system integration tutorial and update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: '独立站教程 | 工具',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.simpleone.fun/',
+  siteRepo: 'https://github.com/j-joker/tailwind-nextjs-starter-blog/',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
