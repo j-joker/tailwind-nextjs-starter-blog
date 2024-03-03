1472dd75d7d4c98af5552b5619fd23f130fa0ee1
June <zlxjune@gmail.com>
2024-03-04 01:39:29 +0800

    Add new header navigation links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,3 +1,7 @@
-const headerNavLinks = [{ href: '/', title: 'Home' }]
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/tool', title: '工具' },
+  { href: '/blog', title: '教程' },
+]
 
 export default headerNavLinks
